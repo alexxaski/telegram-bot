543a91a877e61704826d484669e0dd29999f0992
alexxaski <alexxaski@yandex.ru>
2025-05-18 12:32:08 +0600

    Remove target and .idea directories from repository

--- target/classes/db/changelog/notifications-tasks-table.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
--- changeset 1:create_notification_tasks_table
-CREATE TABLE notification_task (
-    id SERIAL PRIMARY KEY,
-    chat_id BIGINT NOT NULL,
-    notification_text TEXT NOT NULL,
-    scheduled_time TIMESTAMP NOT NULL
-);
